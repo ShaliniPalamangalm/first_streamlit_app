634f3d6455326e57c165f91a41b4c8f668ac58a2
ShaliniPalamangalm <134564920+ShaliniPalamangalm@users.noreply.github.com>
2023-05-25 14:30:48 +1200

    Update streamlit_app.py
    Added New Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberry oatmeal")
 streamlit.text("Kale,Spinach and Rocket Smoothie")
 streamlit.text("Hard boiled free range Menu")               
+streamlit.text("Avacado Toastie")  
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
