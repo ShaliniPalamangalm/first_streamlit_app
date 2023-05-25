4334c68354bf213d12d79a9d96f15b613e47aa2d
ShaliniPalamangalm <134564920+ShaliniPalamangalm@users.noreply.github.com>
2023-05-25 16:11:28 +1200

    Update streamlit_app.py
    adding fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.text("Hard boiled free range Menu")
 streamlit.text("Avacado Toastie")  
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas as pd
+my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
