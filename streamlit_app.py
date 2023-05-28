e8d83357627b9508bbf110cf78a8ea857f2e1117
ShaliniPalamangalm <134564920+ShaliniPalamangalm@users.noreply.github.com>
2023-05-28 19:35:25 +1200

    Update streamlit_app.py
    added connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,4 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choi
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
+import snowflake.connector
